3c69331e5cded6869461a15d1c0ec16fa7f93ed2
onjussss <ojastaneja007@gmail.com>
2025-03-23 23:17:24 +0530

    Update Dataset View.py

--- insurance_dataset_analysis/pages/Dataset View.py
+++ insurance_dataset_analysis/pages/Dataset View.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 st.set_page_config(page_title="Dataset View",page_icon="💾")
 st.title("Dataset View")    
-data=pd.read_csv("insurance.csv")
+data=pd.read_csv("insurance_dataset_analysis/insurance.csv")
 st.write(data)
